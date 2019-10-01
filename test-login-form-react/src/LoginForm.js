2eaf4eddb79cc6742ba963613a2419ddff325200
Artem <ass@gmail.com>
2019-10-02 01:27:04 +0400

    Removed direct call to the DOM elements.

--- test-login-form-react/src/LoginForm.js
+++ test-login-form-react/src/LoginForm.js
@@ -8,12 +8,16 @@ class LoginForm extends React.Component {
 
         this.state = { 
             isLoggedIn: false, 
-            hasError: false
+            hasError: false,
+            email: '',
+            pwd : '',
          };
 
         this.postAjax = this.postAjax.bind(this);
-        this.logIn = this.logIn.bind(this);
-        this.logOut = this.logOut.bind(this);
+        this.doLogIn = this.doLogIn.bind(this);
+        this.doLogOut = this.doLogOut.bind(this);
+        this.onEmailChange = this.onEmailChange.bind(this);
+        this.onPwdChange = this.onPwdChange.bind(this);
     }
 
     render() {
@@ -21,6 +25,14 @@ class LoginForm extends React.Component {
             return this.renderLogOut();
         else
             return this.renderLogIn();
+    }
+
+    onEmailChange(event){
+        this.setState({email: event.target.value});
+    }
+
+    onPwdChange(event){
+        this.setState({pwd: event.target.value});
     }
 
     renderLogIn(){
@@ -37,12 +49,20 @@ class LoginForm extends React.Component {
             <div className="app-container__body login-form panel">
                 <h1 className="login-form__title">Log In</h1>
 
-                <input type="text" id="inputEmail" className={emailClassName} placeholder="E-Mail"/>
-                <input type="text" id="inputPwd" className={pwdClassName} placeholder="Password"/>
+                <input type="text" id="inputEmail" 
+                    value={this.state.email}
+                    className={emailClassName} 
+                    onChange={this.onEmailChange}
+                    placeholder="E-Mail"/>
+                <input type="text" id="inputPwd" 
+                    value={this.state.pwd}
+                    className={pwdClassName} 
+                    onChange={this.onPwdChange}
+                    placeholder="Password"/>
 
                 {errorTag}
 
-                <button id="btnLogIn" className="login-form__submit button" onClick={this.logIn}>
+                <button id="btnLogIn" className="login-form__submit button" onClick={this.doLogIn}>
                     Login
                 </button>
             </div>
@@ -56,16 +76,16 @@ class LoginForm extends React.Component {
 
                 <h1 className="login-form__title">{this.userName}</h1>
 
-                <button id="btnLogOut" className="login-form__submit button" onClick={this.logOut}>
+                <button id="btnLogOut" className="login-form__submit button" onClick={this.doLogOut}>
                     Logout
                 </button>
             </div>
         );
     }
 
-    logIn() {
-        var email = document.getElementById("inputEmail").value;
-        var pwd = document.getElementById("inputPwd").value;
+    doLogIn() {
+        var email = this.state.email;
+        var pwd = this.state.pwd;
 
         this.postAjax(
             this.props.baseUrl,
@@ -73,12 +93,12 @@ class LoginForm extends React.Component {
         );
     }
 
-    logOut(){
+    doLogOut(){
         this.photoUrl = '';
         this.userName = '';
         this.errorMessage = '';
 
-        this.setState({isLoggedIn: false, hasError: false});
+        this.setState({isLoggedIn: false, hasError: false, email: '', pwd: ''});
     }
     
     async postAjax(url, data) {
