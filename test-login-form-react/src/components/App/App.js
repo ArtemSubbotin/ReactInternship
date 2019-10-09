4e5849948776b7a7decd76da2d2104f2b258f70f
Artem <ass@gmail.com>
2019-10-10 01:10:43 +0400

    removed a style hardcode in the index file directly.

--- test-login-form-react/src/components/App/App.js
+++ test-login-form-react/src/components/App/App.js
@@ -6,10 +6,10 @@ import MainContainer from '../MainContainer/MainContainer';
 export default class App extends React.Component {
   render() {
     return (
-      <>
-        <img src={logo} className="app-container__logo" alt="logo" />
+      <div className="app__container">
+        <img src={logo} className="app__logo" alt="logo" />
         <MainContainer baseUrl="https://us-central1-mercdev-academy.cloudfunctions.net/login"/>
-      </>
+      </div>
     );
   }
 }
