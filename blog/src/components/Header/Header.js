a3e9c6d7db609bc0b88a37432b5eb9d2275bbd4d
Artem <ass@gmail.com>
2019-10-21 03:15:06 +0400

    simplification

--- blog/src/components/Header/Header.js
+++ blog/src/components/Header/Header.js
@@ -2,22 +2,20 @@ import React from "react";
 import "./Header.css";
 import SearchField from "../SearchField/SearchField";
 
-export default class Header extends React.Component {
-  render() {
-    return (
-      <div className="header">
-        <div className="header__container">
-          <div className="header__massages-container">
-            <p className="header__massages-title">Messages</p>
-            <span className="header__counter">{this.props.messagesCnt}</span>
-          </div>
-
-          <SearchField
-            onSearchClick={this.props.onSearchClick}
-            className="header__search-field"
-          />
+export default function Header(props) {
+  return (
+    <div className="header">
+      <div className="header__container">
+        <div className="header__massages-container">
+          <p className="header__massages-title">Messages</p>
+          <span className="header__counter">{props.messagesCnt}</span>
         </div>
+
+        <SearchField
+          onSearchClick={props.onSearchClick}
+          className="header__search-field"
+        />
       </div>
-    );
-  }
+    </div>
+  );
 }
--- blog/src/components/Pic/Pic.js
+++ blog/src/components/Pic/Pic.js
@@ -1,21 +1,16 @@
 import React from "react";
 import "./Pic.css";
 
-export default class Pic extends React.Component {
-  render() {
-    return (
-      <div className="pic">
-        <img
-          src={
-            this.props.userUrl ||
-            "https://randomuser.me/api/portraits/lego/1.jpg"
-          }
-          className="pic__img"
-          alt="logo"
-        />
+export default function Pic(props) {
+  return (
+    <div className="pic">
+      <img
+        src={props.userUrl || "https://randomuser.me/api/portraits/lego/1.jpg"}
+        className="pic__img"
+        alt="logo"
+      />
 
-        {this.props.online && <div className="pic__online"></div>}
-      </div>
-    );
-  }
+      {props.online && <div className="pic__online"></div>}
+    </div>
+  );
 }
