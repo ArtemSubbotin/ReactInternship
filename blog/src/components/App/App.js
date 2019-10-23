6202fc9371bcde345e44a22e60d50369c7ec40f5
Artem <ass@gmail.com>
2019-10-24 01:21:04 +0400

    data loading simplified.

--- blog/src/components/App/App.js
+++ blog/src/components/App/App.js
@@ -2,90 +2,36 @@ import React from "react";
 import "./App.css";
 import Post from "../Post/Post";
 import Header from "../Header/Header";
+import { fetchData } from "./DataFetcher";
 
 import { fetchPosts, fetchUsers, getRandomUserPic } from "../../api/api";
-
-function randomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
-}
-
-function randomBool() {
-  return Math.random() >= 0.5;
-}
-
-function pad(val) {
-  return val < 10 ? `0${val}` : `${val}`;
-}
-
-function randomTime() {
-  return `${pad(randomInt(12))}:${pad(randomInt(60))} ${
-    randomBool() ? "AM" : "PM"
-  }`;
-}
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      postsLoaded: false,
-      usersLoaded: false,
-      picsLoaded: false,
-      posts: null,
-      users: null,
-      pics: null,
-      searchText: null,
+      posts: [],
+      searchText: "",
+      loaded: false,
       cnt: 0
     };
 
-    this.fetchData();
+    fetchData().then(posts => {
+      this.setState({
+        posts: posts,
+        cnt: posts.length,
+        loaded: true
+      });
+    });
   }
-
-  async asyncForEach(array, callback) {
-    for (let index = 0; index < array.length; index++) {
-      await callback(array[index], index, array);
-    }
-  }
-
-  fetchData = async () => {
-    let posts = await fetchPosts();
-    posts.forEach(post => {
-      post.time = randomTime();
-    });
-    this.setState({
-      postsLoaded: true,
-      posts: posts,
-      cnt: posts.length
-    });
-
-    let users = {};
-    (await fetchUsers()).forEach(user => {
-      users[user.id] = { id: user.id, name: user.name, online: randomBool() };
-    });
-
-    this.setState({
-      usersLoaded: true,
-      users: users
-    });
-
-    let pics = {};
-    await this.asyncForEach(Object.values(users), async user => {
-      let pic = await getRandomUserPic();
-      pics[user.id] = pic;
-    });
-
-    this.setState({
-      pics: pics,
-      picsLoaded: true
-    });
-  };
 
   onSearchClick = searchText => {
     this.setState({ searchText: searchText });
   };
 
   render() {
-    if (!this.state.postsLoaded) {
+    if (!this.state.loaded) {
       return (
         <div className="app">
           <div className="app__loader">loading...</div>
@@ -111,20 +57,12 @@ export default class App extends React.Component {
           {filteredPosts.map(post => (
             <Post
               key={post.id}
-              userUrl={
-                this.state.picsLoaded ? this.state.pics[post.userId] : null
-              }
+              userUrl={post.user.pic}
               highlightText={this.state.searchText}
-              userName={
-                this.state.usersLoaded
-                  ? this.state.users[post.userId].name
-                  : null
-              }
+              userName={post.user.name}
               title={post.title}
               body={post.body}
-              online={
-                this.state.usersLoaded && this.state.users[post.userId].online
-              }
+              online={post.user.online}
               time={post.time}
             />
           ))}
